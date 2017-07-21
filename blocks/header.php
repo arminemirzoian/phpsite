3ef3400105863ccd854462a45b4e2bb982ddeaf1
Armine <test@test.com>
2017-07-21 18:06:15 +0400

    Header, footer, left side and database connection

---
+++ blocks/header.php
@@ -0,0 +1,9 @@
+<a href="index.php" class="ref"><div id="header">
+</div></a>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MKphoto
+ * Date: 7/13/2017
+ * Time: 1:26 PM
+ */
---
+++ blocks/left.php
@@ -0,0 +1,36 @@
+<div id="left">
+    <p align="center" class="title">Navigation</p>
+    <div id="coolmenu">
+        <a href="index.php">Home</a>
+        <a href="articles.php">Articles</a>
+        <a href="lessons.php">Lessons</a>
+        <a href="contacts.php">About us</a>
+    </div>
+    <p align="center" class="title2">Newsletter</p>
+    <div class="formm" width="80%">
+        <p class='form3'> Subscribe our page and receive new lessons, articles and news directly to your email!</p>
+        <form name="SR_form" method="post" target="_blank" action="http://smartresponder.ru/subscribe.html" onsubmit="return SR_submit(this)">
+            <input type=hidden name=version value="1">
+            <input type=hidden name=tid value="34769">
+            <input type=hidden name=uid value="27025">
+            <input type=hidden name=charset value="windows-1251">
+            <input type=hidden name=lang value="1">
+            <input type=hidden name="did[]" value="9267">
+            <p class='form1'> Name in English:
+                <input type="text" name="field_name_first" size=23 value='' maxlength=50 style='border: 1px #c1c1c1 solid; font-family: Verdana; font-size: 11px; width:120px; color:#424242;'></p>
+            <p class='form1'> Email address:
+                <input type=text name="field_email" size=23 value='' maxlength=50   style='margin:0px; padding:0px; border: 1px #c1c1c1 solid; font-family: Verdana; font-size: 11px; width:120px; color:#424242;'></p>
+            <p style='margin:5px;margin-top:10px; padding:0px;'>
+                <input type="submit" name="SR_submitButton" value='Subscribe' style=' font-family: Verdana, sans-serif; border:1px gray solid; font-size: 11px; width:120px; height:19px;   background-Color:#f6f6f6; color:#424242; font-weight:bold; margin-left:10px;' >
+            </p>
+        </form>
+    </div>
+</div>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MKphoto
+ * Date: 7/13/2017
+ * Time: 1:29 PM
+ */
